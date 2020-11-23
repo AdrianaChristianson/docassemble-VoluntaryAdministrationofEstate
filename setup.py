388f11a26621512f8c36423858ffe5287d7b5d52
Adriana Christianson <69772513+AdrianaChristianson@users.noreply.github.com>
2020-11-23 17:40:26 -0500

    Updated question order

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.VoluntaryAdministrationofEstate',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.VoluntaryAdministrationofEstate\n\nA docassemble extension.\n\n## Author\n\nAdriana Christianson, achristianson@suffolk.edu\n\n',
+      long_description='# docassemble.VoluntaryAdministrationofEstate\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nAdriana Christianson, achristianson@suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Adriana Christianson',
       author_email='achristianson@suffolk.edu',
